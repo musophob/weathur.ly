59aeef4a059b00caa58b37d130aa7117c1d6554e
Sean Henderson <mail@sean.sh>
2016-04-21 19:59:12 -0700

    add ability to get html5 geo coordinates
    * logs latitude/longitude or error status to the console
    * placeholder functions for actually getting the weather

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+
+  function getForecast() {
+
+    if (!navigator.geolocation){
+      console.log("we don't have geo location");
+      return false;
+    }
+
+    function success(position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      console.log(latitude + " " + longitude)
+      // getWeatherByCoords(latitude, longitude)
+    };
+
+    function error() {
+      console.log("geo location error or blocked");
+      // triggerZipInput()
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error);
+
+  }
+
+  getForecast();
+});
+
+
